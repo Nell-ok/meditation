cf13efeaa1fd997b6d937a655c21d195cbb842e8
Nelly <nelli-nn@yandex.ru>
2025-11-07 18:27:42 +0300

    Добавляет базовые стили

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts"></script>
 
-<template></template>
+<template>
+  <p class="title">Добро пожаловать, Наталья!</p>
+</template>
 
-<style scoped></style>
+<style scoped>
+.title {
+  font-family: var(--font-alegreya);
+  font-size: 30px;
+  font-weight: 500;
+  line-height: 41px;
+}
+</style>
